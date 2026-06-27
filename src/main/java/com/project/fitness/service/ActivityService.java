d1217a5c004d719530b46f8914f4feab36b7fca1
khushijha24 <khushiujan2005@gmail.com>
2026-06-27 20:01:51 +0530

    Chnages to ActivityService

--- src/main/java/com/project/fitness/service/ActivityService.java
+++ src/main/java/com/project/fitness/service/ActivityService.java
@@ -1,5 +1,8 @@
 package com.project.fitness.service;
 
+import com.project.fitness.dto.ActivityRequest;
+import com.project.fitness.dto.ActivityResponse;
+import com.project.fitness.model.Activity;
 import com.project.fitness.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +11,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ActivityService {
     private final ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+
+        Activity activity = Activity.builder()
+                .type(request.getType())
+                .
+                .build();
+    }
 }
